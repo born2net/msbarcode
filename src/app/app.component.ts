ee61c957fbad9868f232cdc4ac6a90ed456c480e
root <born2net@gmail.com>
2016-11-30 20:49:10 -0800

    stringjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,30 @@
 import {Component} from '@angular/core';
-import * as _ from 'lodash';
 import {Lib} from "../Lib";
+import * as _ from 'lodash';
+import {Map} from 'immutable';
+
+var notify = function notify(state:Map<string, any> = Map<string, any>({}), action:any) {
+    //console.log('ACTION: ' + action.type);
+    return {data: Map({currentAction: action.type})};
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public title:any = 'app works!';
-  constructor() {
-    setTimeout(()=>{
-      this.title = _.random(1, 51);
-      Lib.test();
-    },1)
-  }
+    public title: any = 'app works!';
+
+    constructor() {
+
+
+        setTimeout(() => {
+            this.title = _.random(1, 51);
+            console.log(StringJS('hello--world').humanize().s);
+            console.log(StringJS('hello--world').camelize().s);
+            console.log(StringJS(123).toCurrency());
+            console.log(StringJS('&$^72y7edg').cleanChar());
+        }, 1)
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { MaterialModule } from '@angular/material';
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {MaterialModule} from '@angular/material';
+import {AppComponent} from './app.component';
 import 'hammerjs';
+import notify from "../reducers/NotifyReducer";
+import {Lib} from "../Lib";
+import {AppStore} from "angular2-redux-util";
+
+export var providing = [
+    {
+        provide: AppStore,
+        useFactory: Lib.StoreFactory({
+            notify
+        })
+    }];
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    MaterialModule.forRoot()
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        MaterialModule.forRoot()
+    ],
+    providers: [providing],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
