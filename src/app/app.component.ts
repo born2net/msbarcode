eafa9a13ff9e80c4eb0667e3339ba6e64fbdedf0
root <born2net@gmail.com>
2016-11-30 17:47:03 -0800

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  public title = 'app works!';
+
+  constructor() {
+    setTimeout(()=>{
+      console.log(_.random(1, 2000));
+      console.log(12);
+      console.log(3);
+    },1)
+
+  }
 }
