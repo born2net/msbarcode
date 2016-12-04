efaece6afb257e0fceb40d2bc7f4a1961d7ba1fe
root <born2net@gmail.com>
2016-12-04 08:09:03 -0800

    currently ng serve failing on [providing] with beta .22

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -37,16 +37,18 @@ import {MsLibModule} from "ng-mslib/dist/mslib.module"; //toggle
  **/
 
 
+var fac = (ngRedux: NgRedux<any>, devTools: DevToolsExtension) => {
+    const reducers = combineReducers({notify, sample_reducer});
+    const middlewareEnhancer = applyMiddleware(<any>thunkMiddleware);
+    const applyDevTools = () => devTools.isEnabled() ? devTools.enhancer : f => f;
+    const enhancers: any = compose(middlewareEnhancer, applyDevTools);
+    const store = createStore(reducers, enhancers);
+    ngRedux.provideStore(store);
+    return new AppStore(store);
+}
+
 export var providing = [{
-    provide: AppStore, useFactory: (ngRedux: NgRedux<any>, devTools: DevToolsExtension) => {
-        const reducers = combineReducers({notify, sample_reducer});
-        const middlewareEnhancer = applyMiddleware(<any>thunkMiddleware);
-        const applyDevTools = () => devTools.isEnabled() ? devTools.enhancer : f => f;
-        const enhancers: any = compose(middlewareEnhancer, applyDevTools);
-        const store = createStore(reducers, enhancers);
-        ngRedux.provideStore(store);
-        return new AppStore(store);
-    }, deps: [NgRedux, DevToolsExtension]
+    provide: AppStore, useFactory: fac, deps: [NgRedux, DevToolsExtension]
 }, {
     provide: "OFFLINE_ENV",
     useValue: false
